b50609fd2a16c464b2e07302ef5bd2e1ae27530c
Raditz <radityohernanda@gmail.com>
2026-06-17 17:40:19 +0700

    improvement(gui): Centralized Delete row button

---
+++ crates/beep-gui/src/lib/components/buttons/DeleteRowButton.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { XIcon } from "@lucide/svelte";
+
+    interface Props {
+        onclick: () => void;
+    }
+
+    let { onclick }: Props = $props();
+</script>
+
+<button
+    class="btn btn-ghost btn-xs text-error opacity-0 group-hover:opacity-100"
+    {onclick}
+>
+    <XIcon class="w-3 h-3" />
+</button>
